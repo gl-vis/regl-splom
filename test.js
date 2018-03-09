8457a219677daff12256fc8146625b9f053488a9
Dmitry <df.creative@gmail.com>
2018-03-08 22:40:49 -0500

    Add test code

--- test.js
+++ test.js
@@ -1,36 +1,67 @@
 'use strict'
 
 const regl = require('regl')({ extensions: 'oes_element_index_uint'})
-const scatter = require('regl-scatter2d')(regl)
-const bounds = require('array-bounds')
+const createSettings = require('settings-panel')
+const createMatrix = require('./')
+const panzoom = require('pan-zoom')
+const random = require('gauss-random')
+const fps = require('fps-indicator')('bottom-right')
 
-const data = new Float32Array([0,.5,1,3, 4,5,6,7, 11,10,9,8])
-const buffer = regl.buffer(data)
 
-let w = regl._gl.drawingBufferWidth
-let h = regl._gl.drawingBufferHeight
-let n = 4
-let m = 3
-let iw = w / m
-let ih = h / m
-let passes = []
-for (let i = 0; i < m; i++) {
-	for (let j = 0; j < m; j++) {
-		let xOffset = i * n
-		let yOffset = j * n
-		let [lox, hix] = bounds(data.subarray(xOffset, xOffset + n))
-		let [loy, hiy] = bounds(data.subarray(yOffset, yOffset + n))
+// create splom instance
+let splom = createMatrix(regl)
 
-		passes.push({
-			positions: {
-				x: {buffer, offset: xOffset, count: n},
-				y: {buffer, offset: yOffset, count: n}
-			},
-			bounds: [lox, loy, hix, hiy],
-			viewport: [i * iw, j * ih, (i + 1) * iw, (j + 1) * ih]
-		})
+
+// data for the splom
+let traces = []
+
+
+// create settings panel & bind
+let settings = createSettings({
+	traces: { value: 3, min: 1, max: 10, type: 'number' },
+	variables: { value: 4, min: 1, max: 100, type: 'number' },
+	points: { value: 1e4, min: 1, max: 1e7, type: 'number' },
+	snap: { value: false }
+}, {
+	position: 'center bottom'
+})
+
+settings.on('change', update)
+
+
+// regenerate the data based on options
+function update () {
+	let o = settings.values
+
+	// set proper number of traces
+	if (o.traces < traces.length) {
+		traces.length = o.traces
 	}
+	else {
+		for (let i = traces.length; i < o.traces; i++) {
+			traces.push(generateTrace(o.variables, o.points))
+		}
+	}
+
+	function generateTrace(vars, points) {
+		let data = []
+		for (let i = 0; i < vars; i++) {
+			let row = []
+			data.push(row)
+			for (let j = 0; j < points; j++) {
+				row.push(random())
+			}
+		}
+
+		return data
+	}
+
+	// update splom based on traces
+	splom.update(...traces)
 }
 
-scatter.update(passes)
-scatter.draw()
+
+// redraw the frame based on data
+function draw () {
+	splom.draw()
+}
